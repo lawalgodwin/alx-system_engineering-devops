1d7276f915d60ea78af89e1c512071cfd0113105
lawalgodwin <godwinl200@gmail.com>
2023-03-24 19:53:10 +0100

    Create file using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# A puppet script that creates a file `School` in /tmp
+
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
