f6570b79c22dd0df21f8dba73d0be4117f6f1250
lawalgodwin <godwinl200@gmail.com>
2023-05-17 13:52:42 +0100

    Optimize nginx web server to accept more requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# A puppet manifest that optimises an nginx webserver to receive more requests
+
+exec { 'increase-number-of-open-file-limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin:/bin',
+  notify  => Service['nginx']
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Exec['increase-number-of-open-file-limit']
+}
