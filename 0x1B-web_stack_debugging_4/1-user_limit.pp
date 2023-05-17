e2a2658c0b80747e4d1f5e659af2d278fe0c88c1
lawalgodwin <godwinl200@gmail.com>
2023-05-17 14:06:51 +0100

    Increase linux user limit on number of open files

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# A puppet manifest that increase the limit on the number
+# of files that can the oepned by a user
+
+exec { 'increase-user-limit-hard':
+  command => 'sed -i -e "s/holberton hard nofile 5/holberton hard nofile 8192/"',
+  path    => '/usr/local/bin:/bin/'
+}
+
+
+exec { 'increase-user-limit-soft':
+  command => 'sed -i -e "s/holberton soft nofile 4/holberton soft nofile 8192/"',
+  path    => '/usr/local/bin:/bin/'
+}
