586058f033c391108a3022e7985dcea5929aeebb
lawalgodwin <godwinl200@gmail.com>
2023-08-08 12:37:22 +0100

    Get the No. subscribers in a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains a function that gets
+   the number of subscibers in a given redit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Get the count of subscribers in the given subreddit"""
+    headers = {'User-Agent': 'MyAPIConsumer/1.0 by lawalgodwin'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers)
+    if response.status_code >= 300:
+        return 0
+    subreddit_info = response.json()
+    subscriber_count = subreddit_info['data']['subscribers']
+    return subscriber_count
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,3 @@
+# API Advance
+- Consuming the Reddit API
+- Setting custom User-Agent Header for API with User-Agent restrictions
