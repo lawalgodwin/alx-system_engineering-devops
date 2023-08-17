34c940307ed97eb4006c0d9ea732f41dd58611a7
lawalgodwin <godwinl200@gmail.com>
2023-08-17 17:46:37 +0100

    Increase the limit of number request a nginx worker process can handle

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
-# A manifest that increases the limit of an Nginx web server to handle more requests(i.e, increase the number of files nginx worker process can open)
+# A puppet manifest that optimises an nginx worker process to handle more requests
 
 exec { 'increase-number-of-open-file-limit':
-  command => 'sed -i "s/15/4096" /etc/default/nginx',
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin:/bin/',
   notify  => Service['nginx']
 }
